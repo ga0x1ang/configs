c14c80e2dc74d3788ad1b10e57bf9640cc636f49
GAO Xiang <patriot7@live.cn>
2014-10-14 18:11:32 -0400

    update make.conf

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
-eval "git clone https://github.com/fatih/vim-go.git"
-eval "git clone https://github.com/scrooloose/nerdcommenter.git"
-eval "git clone https://github.com/altercation/vim-colors-solarized.git"
+git clone https://github.com/fatih/vim-go.git
+git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/altercation/vim-colors-solarized.git
 git clone git://github.com/tpope/vim-sensible.git
 cp -r ./etc /etc
 cp ./home/vimrc $HOME/.vimrc

877e0ec23003805a2ecff19f0fd6587feda4caed
GAO Xiang <patriot7@live.cn>
2014-10-23 15:52:48 -0400

    added more rc files; now vim-go tools are installed automatically

--- README.md
+++ README.md
@@ -2,4 +2,13 @@ NOTES
 =====================
 - current st ebuild doesn't have patch command, add it manually to apply the solarized-scheme
 - dwm xft patch doesn't work (pango also doesn't work, need to look at the patch file and the code fetched by ebuild)
-- some binary tools needs to be installed mannually after install vim-go (GoUpdateBinaries in vim)
+
+
+profiles:
+```
+gentoo:funtoo/1.0/linux-gnu/arch/x86-64bit /* r60 :\ */
+gentoo:funtoo/1.0/linux-gnu/build/current
+gentoo:funtoo/1.0/linux-gnu/flavor/core
+gentoo:funtoo/1.0/linux-gnu/mix-ins/audio
+gentoo:funtoo/1.0/linux-gnu/mix-ins/X
+```
--- setup.sh
+++ setup.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+# install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# install plugins
 cd ~/.vim/bundle
 git clone https://github.com/fatih/vim-go.git
 git clone https://github.com/scrooloose/nerdcommenter.git
 git clone https://github.com/altercation/vim-colors-solarized.git
 git clone git://github.com/tpope/vim-sensible.git
+
+# rc files (i really should learn some bash scripting ... :\)
+cd ~
 cp -r ./etc /etc
 cp ./home/vimrc $HOME/.vimrc
 cp ./home/gitconfig $HOME/.gitconfig
+cp ./home/conkyrc $HOME/.conkyrc
+cp ./home/xinitrc $HOME/.xinitrc
+cp ./home/bashrc $HOME/.bashrc
+
+mkdir golang
+
+vim +GoUpdateBinaries +qall
